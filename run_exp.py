4ebae525c5c0e5c7a6f0548f9e0e7f5019a5e510
YifanYang6 <yifanyang6@link.cuhk.edu.cn>
2025-08-03 23:59:57 +0800

    fix entrypoint
    Signed-off-by: YifanYang6 <yifanyang6@link.cuhk.edu.cn>

--- run_exp.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S uv run -s
-from rcabench_platform.v2.online.run_exp_platform import run
-from rcabench_platform.v2.algorithms.spec import global_algorithm_registry
-from rcaeval_causalrca.causalrca import CausalRCA
-
-if __name__ == "__main__":
-    registry = global_algorithm_registry()
-    registry["causalrca"] = CausalRCA
-    run(enable_builtin_algorithms=False)
